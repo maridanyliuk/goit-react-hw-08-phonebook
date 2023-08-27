94225d346ef1c388fee9e04c6fd40e1a19f51c19
maridanyliuk <113414981+maridanyliuk@users.noreply.github.com>
2023-08-27 11:22:12 +0000

    Deploying to gh-pages from @ maridanyliuk/goit-react-hw-08-phonebook@1e541eb2978cb1f02cf3be7005017b3e22c05143 🚀

---
+++ static/js/853.9dbb13c2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[853],{3853:function(t,e,r){r.d(e,{fzv:function(){return o}});var n=r(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"}},{tag:"circle",attr:{cx:"12",cy:"7",r:"4"}}]})(t)}}}]);
+//# sourceMappingURL=853.9dbb13c2.chunk.js.map
